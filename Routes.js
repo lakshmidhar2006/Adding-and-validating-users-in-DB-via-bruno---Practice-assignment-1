fe6edf95e825bcd242211ac432291e0661eb10d9
unknown <santimallalakshmidhar@gmail.com>
2025-04-02 11:41:06 +0530

    first commit

--- Routes.js
+++ Routes.js
@@ -26,4 +26,27 @@ catch(e){
   res.status(400).send({e})
 }
 })
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "not found" });
+        }
+
+        const check = await bcrypt.compare(password, user.password);
+        if (!check) {
+            return res.status(401).json({ message: "wrong credentials" });
+        }
+
+        res.status(200).json({ message: " successful", user });
+    } catch (error) {
+        res.status(500).json({ message: "server Error", error });
+    }
+});
 module.exports=router
